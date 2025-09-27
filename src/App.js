7d3b62c25b671966ac8ab8dff762d892406628bf
Madhurika17 <119500527+Madhurika17@users.noreply.github.com>
2025-09-27 11:52:18 +0000

    feat(ui): ingredient input + home skeleton

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './index.css'; // make sure this points to your Tailwind-imported CSS
+import React, { useState } from "react";
+import IngredientInput from "./components/IngredientInput";
 
 function App() {
+  const [ingredients, setIngredients] = useState([]);
+
+  const handleSearch = (ingArray) => {
+    setIngredients(ingArray);
+    console.log("Searching for recipes using:", ingArray);
+  }
+
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
-      <header className="flex flex-col items-center">
-        <img src={logo} className="w-32 h-32 animate-spin" alt="logo" />
-        <p className="mt-4 text-lg text-gray-700">
-          Edit <code className="bg-gray-200 p-1 rounded">src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="mt-2 text-blue-600 hover:underline"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="p-8 max-w-3xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-center">Cook & Time</h1>
+      <IngredientInput onSearch = {handleSearch}/>
+
+      <div className='mt-6'>
+        {ingredients.length > 0 && (
+          <p className='text-grey-700'>
+            You searched for: <strong>{ingredients.join(",")}</strong>
+          </p>
+        )}
+      </div>
+      
     </div>
   );
 }
